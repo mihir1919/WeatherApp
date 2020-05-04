a11e022851fe7138e87e86ea0701556bdf0f04e7
mihir <mihirsri10@gmail.com>
2020-05-04 19:41:34 +0530

    d
    d

--- track/models.py
+++ track/models.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.db import models
 # Create your models here.
+
+
 class Location(models.Model):
-    Country = models.CharField( max_length=50)
+    Country = models.CharField( max_length=50) 
 
     def __str__(self):
         return self.Country
--- track/views.py
+++ track/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import LocationForm
+from .models import Location
 import requests
 
 # Create your views here.
@@ -7,15 +8,24 @@ def index(request):
     return render(request,'track/index.html',{})
 
 def tracker(request):
+    n = Location.objects.all()
     if request.method == "POST":
         form = LocationForm(request.POST)
+        if form.POST.get("Country") not in n:
+            form.save()
         if form.is_valid():
             url= 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=2edc88045665cd7d7e9ce52053e0948a'
             city=(request.POST.get("Country"))
             r = requests.get(url.format(city)).json()
+            w_d = {
+                'city' : city,
+                'temperature' : r['main']['temp'],
+                'icon' : r['weather'][0]['icon'],
+                'description' : r['weather'][0]['description']
+            }
             print(r)
-            form = r
-            return render(request,'track/result.html',{'form':form})
+            form = w_d
+            return render(request,'track/result.html',{'form':form,'n':n})
     else:
         form = LocationForm()
     return render(request, 'track/tracker.html', {'form': form})
