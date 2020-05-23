30e535563bd8a5faa38e144427057186ba54f9d6
Mihir Srivastava <55758677+mihir1919@users.noreply.github.com>
2020-05-23 20:20:39 +0530

    Added Resources in all disctricts of India

--- track/urls.py
+++ track/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('tracker/',views.tracker,name="tracker"),
     path('tes/',views.tes,name="tes"),
+    path('res/',views.res,name="res"),
+    path('res/<str:city>/', views.resources,name="resources"),
 ]
