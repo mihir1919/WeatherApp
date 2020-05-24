50c96586477eb5f705f7a9c128b122930edaaf91
Mihir Srivastava <55758677+mihir1919@users.noreply.github.com>
2020-05-24 15:48:09 +0530

    added state api

--- track/urls.py
+++ track/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('res/',views.res,name="res"),
     path('res/<str:city>/', views.resources,name="resources"),
     path('yo/<str:city>/',views.my_view,name="yo"),
+    path('sup/<str:state>/',views.dis_view,name="sup"),
 ]
