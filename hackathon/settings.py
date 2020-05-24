8e557dd1edc9855662c1d289ad249dca97bb1f3e
Mihir Srivastava <55758677+mihir1919@users.noreply.github.com>
2020-05-24 16:13:54 +0530

    Update settings.py

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o!xdj8tpqncs-_uo#fpz7oo_6i1s!_@i-0s3q)fw@3qj*^1^(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
