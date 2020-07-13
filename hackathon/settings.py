240074956f2f4d65c8e7872a56300bf840abe019
mihir <mihirsri10@gmail.com>
2020-07-13 11:49:21 +0530

    debug

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o!xdj8tpqncs-_uo#fpz7oo_6i1s!_@i-0s3q)fw@3qj*^1^(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mihir19.pythonanywhere.com','localhost','127.0.0.1']
 
@@ -118,9 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-
+STATIC_URL = '/track/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- track/views.py
+++ track/views.py
@@ -272,7 +272,7 @@ def normtwitter(request):
             d[i]=l
             i+=1
     word_cloud  = WordCloud(width=800,height=800,background_color='white',stopwords=stopwords,min_font_size=10).generate(stri)
-    word_cloud.to_file('track/static/Timage.png')
+    word_cloud.to_file('track/static/images/Timage.png')
     t=i-1
     return render(request,'track/nt.html',{'d':d,'s':request.POST.get('topic'),'t':t,'labels': labels,
         'data': data})
