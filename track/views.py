ea54b02998208fbfd4e05d1e741b2310c2f92e74
mihir <mihirsri10@gmail.com>
2020-11-22 00:52:13 +0530

    f

--- track/views.py
+++ track/views.py
@@ -334,8 +334,8 @@ def twitter(request):
         d[i]=l
         i+=1
     word_cloud  = WordCloud(width=800,height=800,background_color='white',stopwords=stopwords,min_font_size=10).generate(stri)
-    word_cloud.to_file(BASE_DIR+'track/static/images/Timage.png')
-    # word_cloud.to_file('/home/mihir19/mihir19.pythonanywhere.com/track/static/images/Timage.png')
+    # word_cloud.to_file(BASE_DIR+'track/static/images/Timage.png')
+    word_cloud.to_file('/home/mihir19/mihir19.pythonanywhere.com/track/static/images/Timage.png')
     t=i-1
     print(data)
     print(labels)
@@ -374,8 +374,8 @@ def normtwitter(request):
             d[i]=l
             i+=1
     word_cloud  = WordCloud(width=800,height=800,background_color='white',stopwords=stopwords,min_font_size=10).generate(stri)
-    word_cloud.to_file('track/static/images/Timage.png')
-    # word_cloud.to_file('/home/mihir19/mihir19.pythonanywhere.com/track/static/images/Timage.png')
+    # word_cloud.to_file('track/static/images/Timage.png')
+    word_cloud.to_file('/home/mihir19/mihir19.pythonanywhere.com/track/static/images/Timage.png')
     t=i-1
     return render(request,'track/nt.html',{'d':d,'s':request.POST.get('topic'),'t':t,'labels': labels,
         'data': data})
